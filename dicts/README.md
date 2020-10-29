c65b36b474d38bc229b5f3e4785cc591c4b32972
elleven11 <elleven11@github.com>
2020-10-29 16:59:21 -0400

    working basic program

---
+++ dicts/README.md
@@ -0,0 +1,3 @@
+# rockyou.txt
+you can find the rockyou word list here:  
+https://github.com/brannondorsey/naive-hashcat/releases/tag/data
--- main.go
+++ main.go
@@ -1,25 +1,71 @@
 package main
 
 import (
+	"bufio"
 	"crypto/md5"
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
-	plaintext := "hello"
-
-	// MD5 values are often printed in hex, for example
-	// in git commits. Use the `%x` format verb to convert
-	// a hash results to a hex string.
-	fmt.Println(plaintext)
-	fmt.Printf("%x\n", hash(plaintext))
+	createHashDict("test.txt", readLines("dicts/rockyou.txt"))
 }
 
-func hash(plaintext string) string {
+func createHashDict(fileName string, plaintextDict []string) {
+	fmt.Printf("Creating hashed dictionary\n")
+	file, err := os.Create(fileName)
+
+	if err != nil {
+		log.Fatalf("Failed creating file: %s", err)
+	}
+
+	defer file.Close()
+
+	for i := 0; i < len(plaintextDict)-1; i++ {
+		// hash values are often printed in hex, for example
+		// Use the `%x` format verb to convert
+		// a hash results to a hex string.
+		n, err := file.WriteString(hasher(plaintextDict[i]))
+
+		if err != nil {
+			log.Fatalf("Failed writing to file: %s %d", err, n)
+		}
+	}
+
+	fmt.Printf("\nSuccessfully created hashed dictionary!")
+	fmt.Printf("\nFile Name: %s", file.Name())
+}
+
+func readLines(fileName string) []string {
+	file, err := os.Open(fileName)
+
+	if err != nil {
+		log.Fatalf("Failed opening file: %s", err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var txtlines []string
+
+	for scanner.Scan() {
+		txtlines = append(txtlines, scanner.Text())
+	}
+
+	file.Close()
+
+	// for _, eachline := range txtlines {
+	// fmt.Println(eachline)
+	// }
+
+	return txtlines
+}
+
+func hasher(plaintext string) string {
 	h := md5.New()
 
 	h.Write([]byte(plaintext))
 
 	ciphertext := h.Sum(nil)
-	return string(ciphertext)
+	return fmt.Sprintf("%x\n", string(ciphertext))
 }
